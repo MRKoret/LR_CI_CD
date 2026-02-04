1d0deca9c87deea9c2a235ac582849307d546460
sasha <saninazlata1@gmail.com>
2026-02-04 05:23:35 +0300

    mm

--- src/car.py
+++ src/car.py
@@ -1,4 +1,4 @@
-class Car:
+12345465class Car:
     def __init__(self, model: str, fuel_capacity: float) -> None:
         self._model = model
         self._max_fuel_capacity: float = fuel_capacity
